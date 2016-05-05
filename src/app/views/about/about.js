9f07bf63a0c51e20a57258b4015a2264026a2574
Will Thirkettle <hello@willthirkettle.co.uk>
2016-05-05 21:13:34 +0100

    Add events data

--- src/app/views/about/about.js
+++ src/app/views/about/about.js
@@ -17,8 +17,6 @@ module.exports = angular.module('myApp.views.about', [
 ])
 .directive('myViewAbout', function (
 ) {
-	var PLACEHOLDER_IMAGE_SRC = '/images/family_still.jpg';
-	var ANIMATED_IMAGE_SRC = '/images/family_animation.gif';
 
 	return {
 		restrict: 'E',
@@ -28,24 +26,6 @@ module.exports = angular.module('myApp.views.about', [
 		scope: {
 		},
 		link: function (scope, elem, attrs, controller) {
-
-			function _loadImages () {
-				// The animated gif is expensive (3MB) so initially render the image
-				// with a smaller jpg placeholder until we have loaded the gif.
-				var animatedImage;
-				// set placeholder image src
-				scope._familyImageSrc = PLACEHOLDER_IMAGE_SRC;
-				// create new image to load gif
-				animatedImage = new Image();
-				animatedImage.src = ANIMATED_IMAGE_SRC;
-				animatedImage.onload = function () {
-					// switch the image src to the animated gif
-					scope._familyImageSrc = ANIMATED_IMAGE_SRC;
-					scope.$digest();
-				};
-			}
-
-			_loadImages();
 		}
 	};
 })
